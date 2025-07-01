b5f2af369697977d9b37f9693620b74ff5fa9017
Margub Murshed <murshedmargub@gmail.com>
2025-07-01 06:11:28 +0600

    Project initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="p-5">
+      <Button>This is a button</Button>
+    </div>
+  );
+};
+
+export default App;
